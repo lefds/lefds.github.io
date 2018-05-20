ddcf540b1a1e85af96a3cd49ba5ec0a6582887cd
Luís Eduardo Faria dos Santos <39340877+lefds@users.noreply.github.com>
2018-05-20 14:59:59 +0100

    Direto
    s

--- extensions/extension.js
+++ extensions/extension.js
@@ -28,12 +28,9 @@ new (function() {
 	//Attempting to use the jQuery library to load the MQTT client code ...
 	//Unbelivable that this critical step is not reported anywhere!!!	
 	//$.getScript("http://lefds.github.io/extensions/paho-mqtt.js");
-	
 	//$.getScript("https://raw.githubusercontent.com/eclipse/paho.mqtt.javascript/master/src/mqttws31.js");
-	
-	$.getScript("ws://test.mosquitto.org:8080/mqtt");
-	
-	//vendor/mqttws31.js"
+	//$.getScript("ws://test.mosquitto.org:8080/mqtt");	
+	$.getScript("vendor/mqttws31.js");
 	
 	//Inspiration: https://gist.github.com/jpwsutton/6427e38dd3d1db6ba11e48eb0712cba7 => example.js
 	// Create a client instance
