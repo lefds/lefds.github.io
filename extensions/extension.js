504b01328374ca1cc105c9fe47a3a7fbdb71772d
Luís Eduardo Faria dos Santos <39340877+lefds@users.noreply.github.com>
2018-05-20 09:58:02 +0100

    retirei pois não deu

--- extensions/extension.js
+++ extensions/extension.js
@@ -9,10 +9,10 @@
  
 */
 
-
+/*
 <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.js" type="text/javascript">
 </script>
-	
+*/
 /*
 Original Reporter block que devolve o cálculo do argumento
 */	
