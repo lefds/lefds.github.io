f7c1c23a086d4d6d1c7a476b749fa7636da95ce9
Luís Eduardo Faria dos Santos <39340877+lefds@users.noreply.github.com>
2018-05-21 20:24:35 +0100

    inspierado na extensão Mesh
    Na reunião da Sara

---
+++ extensions/extension2.js
@@ -0,0 +1,36 @@
+(function(ext) {
+    // TODO: public repo + documentation + samples
+    // GH pages
+    $.ajax({
+
+        async:false,
+
+        type:'GET',
+
+        url:'http://lefds.github.io/extensions/paho-mqtt.js',
+
+        data:null,
+        
+        success: function(){client = new Paho.MQTT.Client("127.0.0.1", Number(1883), "LSANTOS123");console.log('ok');}, //Create a MQTT reference
+
+        dataType:'script'
+
+    });
+    window['temp'] = 0; // init
+    
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+    
+    ext.broadcast = function(name) {
+        if (name.length > 0){ // blank broadcasts break firebase - not nice.
+        window['sent-' + name] = Math.random(); // HUGE thanks to the folks at White Mountain Science for fixing the multiple broadcast bug! (lines 32-40)
+        console.log(""Fez algo); //Change value of broadcast so other clients get an update
+        }
+    };
+    
