f60d35a33edeb14e9b614403e11a9091dec4fa54
Luís Eduardo Faria dos Santos <39340877+lefds@users.noreply.github.com>
2018-06-03 20:42:47 +0100

    sdsdsd

--- extensions/extension8.js
+++ extensions/extension8.js
@@ -91,8 +91,9 @@
 			success: function(){
 				client = new Paho.MQTT.Client(mqtt_server, mqtt_port, mqttClientID);
 				console.log('MQTT Client handle created');
-				client.onConnectionLost = ajax_onConnectionLost ;	
-				client.connect({onSuccess: ajax_success_onConnect},{onFailure: ajax_success_onConnectError});
+				client.onConnectionLost = ajax_onConnectionLost;
+				client.connect({onFailure: ajax_success_onConnectError});
+				//client.connect({onSuccess: ajax_success_onConnect},{onFailure: ajax_success_onConnectError});
 				connect_status_callback(1);
 			},
 			
