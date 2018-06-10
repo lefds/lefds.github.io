2aaf05707a784a21288f2e52cb95aafc56997475
Luís Eduardo Faria dos Santos <39340877+lefds@users.noreply.github.com>
2018-06-10 08:26:45 +0100

    asdasd

--- extensions/extension11.js
+++ extensions/extension11.js
@@ -105,7 +105,7 @@
 	
 	// ext.WhenLightningController = async function(mqtt_server, mqtt_port) {
 		
-	ext.WhenLightningController1 = async function() {
+	ext.WhenLightningController1 = function() {
 		
 	   console.log("When_WhenLightningController1 beeing called");
        if (alarm_went_off === true) {
